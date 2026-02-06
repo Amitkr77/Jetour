96370832b5bb3a6a7d3cf217e836fe914b22c69e
Amitkr77 <115160075+Amitkr77@users.noreply.github.com>
2026-02-06 11:30:16 +0530

    Fix typo in response message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Jetuor backend running 🚗🔥");
+    res.send("Jetour backend running 🚗🔥");
 });
 
 const PORT = process.env.PORT || 3000;
