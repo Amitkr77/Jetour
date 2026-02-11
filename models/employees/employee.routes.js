7a36d9597b943d280f0e8315050ae0aa9fb61eaf
Amitkr77 <amitroyk99@gmail.com>
2026-02-11 23:06:42 +0530

    removed adin athentication

--- models/employees/employee.routes.js
+++ models/employees/employee.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const controller = require('./employee.controller');
 const {adminAuth} = require('../admin/admin.middleware')
 
-router.get('/technicians', adminAuth, controller.getAllTechnicians);
+// router.get('/technicians', adminAuth, controller.getAllTechnicians);
+router.get('/technicians', controller.getAllTechnicians);
+
 router.get('/technicians/:id', controller.getTechnicianById);
 router.put('/technicians/:id', controller.updateTechnician);
 router.delete('/technicians/:id', controller.deleteTechnician);
