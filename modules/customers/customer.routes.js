28239113381f2e2b30f8e88510ffb72e81bfdcaf
Amitkr77 <amitroyk99@gmail.com>
2026-02-15 10:06:36 +0530

    removed admin athentication for temporary purpose

--- modules/customers/customer.routes.js
+++ modules/customers/customer.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controller = require('./customer.controller');
 const { protect } = require('../../middlewares/auth.middleware');
 
-router.post('/', protect, controller.createCustomer);
-router.get('/', protect, controller.getAllCustomers);
-router.get('/:id', protect, controller.getCustomerDetail);
-router.put('/:id', protect, controller.updateCustomer);
-router.delete('/:id', protect, controller.deleteCustomer);
+router.post('/', controller.createCustomer);
+router.get('/', controller.getAllCustomers);
+router.get('/:id', controller.getCustomerDetail);
+router.put('/:id', controller.updateCustomer);
+router.delete('/:id',  controller.deleteCustomer);
 
 module.exports = router;
--- modules/technician/technician.routes.js
+++ modules/technician/technician.routes.js
@@ -4,10 +4,10 @@ const controller = require('./technician.controller');
 const { protect } = require('../../middlewares/auth.middleware');
 
 // All routes protected
-router.post('/', protect, controller.createTechnician);
-router.get('/', protect, controller.getAllTechnicians);
-router.get('/:id', protect, controller.getTechnicianDetail);
-router.put('/:id', protect, controller.updateTechnician);
-router.delete('/:id', protect, controller.deleteTechnician);
+router.post('/', controller.createTechnician);
+router.get('/', controller.getAllTechnicians);
+router.get('/:id', controller.getTechnicianDetail);
+router.put('/:id',  controller.updateTechnician);
+router.delete('/:id', controller.deleteTechnician);
 
 module.exports = router;
