9d282ec178a5276368fe28d53a80f429de82d02e
Amitkr77 <amitroyk99@gmail.com>
2026-03-05 21:37:45 +0530

    added new route

--- modules/customers/vehicle/customerVehicle.routes.js
+++ modules/customers/vehicle/customerVehicle.routes.js
@@ -5,5 +5,6 @@ const controller = require("./customerVehicle.controller");
 router.post("/", controller.createCustomerVehicle);
 router.put("/:id", controller.updateCustomerVehicle);
 router.delete("/", controller.deleteCustomerVehicle);
+router.get("/", controller.getCustomerVehicles);
 
 module.exports = router;
