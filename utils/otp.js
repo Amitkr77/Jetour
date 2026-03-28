f97a0704a33a0279a2144b77005e4e78d2ae8caa
Amitkr77 <amitroyk99@gmail.com>
2026-03-28 23:59:40 +0530

    refactor: add a blank line for improved code readability in otp.js

--- utils/otp.js
+++ utils/otp.js
@@ -5,6 +5,7 @@ const OTP_EXPIRY_MINUTES = 10;
 /**
  * Generates a 6-digit OTP and its expiry timestamp
  */
+
 const generateOTP = () => ({
   otp: crypto.randomInt(100000, 999999).toString(),
   expiresAt: new Date(Date.now() + OTP_EXPIRY_MINUTES * 60 * 1000)
