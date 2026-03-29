c570526d9641e39cb96876086242003b19bcc19f
Amitkr77 <amitroyk99@gmail.com>
2026-03-29 14:05:59 +0530

    fix: update getHistory to use _id instead of booking_id for consistency

--- modules/driver/driverApp/driverApp.controller.js
+++ modules/driver/driverApp/driverApp.controller.js
@@ -514,7 +514,7 @@ exports.getHistory = async (req, res) => {
 
         const bookings = await Booking.find(filter)
             .select({
-                booking_id: 1,
+                _id: 1,
                 status: 1,
                 schedule: 1,
                 package: 1,
@@ -532,7 +532,7 @@ exports.getHistory = async (req, res) => {
 
         // format response
         const formattedData = bookings.map(b => ({
-            booking_id: b.booking_id,
+            booking_id: b._id,
 
             package_name: b.package?.name,
 
